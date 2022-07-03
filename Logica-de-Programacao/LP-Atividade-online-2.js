6847a148f39adf6146df420368c3b9a5b1e3f13c
cleverlmf <clf.nameless@gmail.com>
2022-07-02 23:55:11 -0300

    m

--- Logica-de-Programacao/LP-Atividade-online-2.js
+++ Logica-de-Programacao/LP-Atividade-online-2.js
@@ -1,21 +1,19 @@
 var listaParticipantes = [];
-var qtdParticipantes = 1;
 
 do {
     var cadastroParticipante = prompt("Informe seu nome para cadastro:");
     var dataEvento = new Date(Date.parse(prompt("Informe a data(mm-dd-yyyy) do evento:")));
     var dataAtual = new Date();
     if (dataEvento > dataAtual) {
-        alert("Cadastro permitido");
+
         var idadeParticipante = prompt("Informe sua idade:");
         if (idadeParticipante > 18) {
-            alert("Cadastro permitido");
 
-            if (listaParticipantes.length < 100) {
-                alert("Cadastro permitido");
-                
+            if (listaParticipantes.length < 2) {
+
                 listaParticipantes.push(cadastroParticipante);
-                
+                alert("Cadastro realizado!");
+
             } else {
                 alert("Cadastro não será permitido por  ter excedido o limite.");
             }
@@ -26,6 +24,6 @@ do {
         alert("O cadastro não será permitido por data inválida.");
     }
 
-  alert("Lista: " + listaParticipantes); 
-  qtdParticipantes++;
-} while (qtdParticipantes < 100); 
+    alert("Lista: " + listaParticipantes);
+
+} while (listaParticipantes.length < 2); 
